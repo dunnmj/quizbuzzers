a947be136bd38d7af986ab908c3c758ba18b19cb
dunnmj <mat@dunnmj.co.uk>
2019-11-08 20:15:15 +0000

    Initial commit

---
+++ quiz.py
@@ -0,0 +1,24 @@
+from gpiozero import LED, Button
+from signal import pause
+from time import sleep
+
+resetbutton = Button(17)
+resetled = LED(18)
+
+b1 = Button(21)
+b2 = Button(20)
+b3 = Button(16)
+b4 = Button(12)
+b5 = Button(7)
+b6 = Button(8)
+b7 = Button(25)
+b8 = Button(24)
+
+l1 = LED(26)
+l2 = LED(19)
+l3 = LED(13)
+l4 = LED(6)
+l5 = LED(5)
+l6 = LED(11)
+l7 = LED(9)
+l8 = LED(10)
